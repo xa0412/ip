9043a424e59cefb5fdd3021bbcdb53f82608dc83
xa0412 <xiaoao1999@gmail.com>
2025-01-18 13:44:53 +0800

    Complete Level 1. Echo

--- src/main/java/xan.java
+++ src/main/java/xan.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class xan {
     public static void main(String[] args) {
         System.out.println("Hello! I'm xan!");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner sc = new Scanner(System.in);
+        String chat = "";
+        while (!chat.equals("bye")) {
+            chat = sc.nextLine();
+            if (!chat.equals("bye")) {
+                System.out.println(chat);
+            }
+        }
+        System.out.println("bye. Hope to see you again!");
     }
 }
