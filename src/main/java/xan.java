5bf78fc0c9d943a50bf4ba1e36d8d477bee5ec28
xa0412 <xiaoao1999@gmail.com>
2025-01-18 13:20:08 +0800

    Complete Level 0. Rename, Greet, Exit

---
+++ src/main/java/xan.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class xan {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm xan!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
