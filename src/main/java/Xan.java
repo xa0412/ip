6ecbb5362eeffe2b91fa154ce9dad3da1da8c7c3
xa0412 <xiaoao1999@gmail.com>
2025-01-19 15:59:37 +0800

    create method for showList and addList, also updated main method, donw with level 2. Add, List

--- src/main/java/Xan.java
+++ src/main/java/Xan.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Xan {
+    private static final ArrayList<String> listArray =  new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Xan!");
         System.out.println("What can I do for you?");
@@ -8,11 +11,26 @@ public class Xan {
         String chat = "";
         while (true) {
             chat = sc.nextLine();
-            System.out.println(chat);
             if (chat.equals("bye")) {
+                System.out.println("bye. Hope to see you again!");
                 break;
             }
+            else if (chat.equals("list")) {
+                showList();
+            } else {
+                addList(chat);
+            }
         }
-        System.out.println("bye. Hope to see you again!");
+    }
+
+    private static void showList() {
+        for (int i = 0; i < listArray.size(); i++) {
+            System.out.println((i + 1) + ". " + listArray.get(i));
+        }
+    }
+
+    private static void addList(String chat) {
+        listArray.add(chat);
+        System.out.println("added: " + chat);
     }
 }
