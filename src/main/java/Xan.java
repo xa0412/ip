38d32d5a0d3ac2f962ebd815da04b3fbd22e57ad
xa0412 <xiaoao1999@gmail.com>
2025-01-19 17:03:40 +0800

    Done with Level3. Mark as done. Add new class Task to help with my main class (Xan) in task marking

--- src/main/java/Xan.java
+++ src/main/java/Xan.java
@@ -2,21 +2,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Xan {
-    private static final ArrayList<String> listArray =  new ArrayList<>();
+    private static final ArrayList<Task> listArray =  new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Xan!");
+        System.out.println("Hello! I'm Xan! I am your personalized Task manager!");
         System.out.println("What can I do for you?");
         Scanner sc = new Scanner(System.in);
         String chat = "";
+
         while (true) {
             chat = sc.nextLine();
             if (chat.equals("bye")) {
                 System.out.println("bye. Hope to see you again!");
                 break;
-            }
-            else if (chat.equals("list")) {
+            } else if (chat.equals("list")) {
                 showList();
+            } else if (chat.startsWith("mark")) {
+                markTask(chat);
+            } else if (chat.startsWith("unmark")) {
+                unmarkTask(chat);
             } else {
                 addList(chat);
             }
@@ -24,13 +28,32 @@ public class Xan {
     }
 
     private static void showList() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < listArray.size(); i++) {
-            System.out.println((i + 1) + ". " + listArray.get(i));
+            Task task = listArray.get(i);
+            System.out.println((i + 1) + "." + task.toString());
         }
     }
 
     private static void addList(String chat) {
-        listArray.add(chat);
+        Task task = new Task(chat);
+        listArray.add(task);
         System.out.println("added: " + chat);
+    }
+
+    private static void markTask(String chat) {
+        int taskIndex = Integer.parseInt(chat.split(" ")[1]) - 1;
+        Task task = listArray.get(taskIndex);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println((taskIndex + 1) + "." + task.toString());
+    }
+
+    private static void unmarkTask(String chat) {
+        int taskIndex = Integer.parseInt(chat.split(" ")[1]) - 1;
+        Task task = listArray.get(taskIndex);
+        task.markAsNotDone();
+        System.out.println("Ok, I've marked this task as not done:");
+        System.out.println((taskIndex + 1) + "." + task.toString());
     }
 }
