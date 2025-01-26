0e215560c3f387a7a420310c30692b0a40293332
xa0412 <xiaoao1999@gmail.com>
2025-01-27 00:01:51 +0800

    Update JavaDoc for XanException class

--- src/main/java/xan/exception/XanException.java
+++ src/main/java/xan/exception/XanException.java
@@ -1,5 +1,8 @@
 package xan.exception;
 
+/**
+ * Represents a custom exception for validation errors in task inputs.
+ */
 public class XanException extends IllegalArgumentException {
     public XanException(String message) {
         super(message);
