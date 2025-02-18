9f3cc7ef2ee8c915a21baf316ce0d7b8aeb71ec9
xa0412 <xiaoao1999@gmail.com>
2025-02-18 12:31:11 +0800

    Update Ui class: Correctly discribed the command message to allow user to input commond

--- src/main/java/ui/Ui.java
+++ src/main/java/ui/Ui.java
@@ -36,9 +36,9 @@ public class Ui {
                   - todo: tasks without any date/time attached to it.
                                   (e.g.todo visit new theme park)
                   - deadline: tasks that need to be done before a specific date/time.
-                                  (e.g. deadline return book / by 2019 - 12 - 01)
+                                  (e.g. deadline return book /by 2019-12-01)
                   - event: tasks that start at a specific date/time and end at a specific date/time.
-                                  (e.g. event project meeting /from Mon 2 pm / to 4 pm)
+                                  (e.g. event project meeting /from Mon 2pm /to 4pm)
                   - delete: to delete a task. (e.g. delete 1)
                   - mark: to mark a task as completed. (e.g. mark 1)
                   - unmark: to unmark a task as not completed. (e.g. unmark 1)
